c2b73aa9afe70aff8b4982f81ea8d582b9d047ef
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-11-19 20:46:52 -0800

    Fixed serverSocket errors

--- server.js
+++ server.js
@@ -9,17 +9,30 @@ const NPCEngine = require('./controllers/NPCEngine')(io);
 const socketConnections = require('./controllers/socket')(io);
 
 const PORT = process.env.PORT || 3001;
-console.log(process.env.TEST_URL);
+console.log(process.env.PUBLIC_URL);
 
-if (process.env.NODE_ENV === 'development') {
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/client/public/index.html'))
-    })
+if (process.env.PUBLIC_URL === '') {
+    if (process.env.NODE_ENV === 'development') {
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, '/client/public/index.html'))
+        })
 
-    server.listen(PORT, () => {
-        console.log('Server listening on http://localhost:' + PORT);
-    });
-} else if (process.env.NODE_ENV === 'production') {
+        server.listen(PORT, () => {
+            console.log('Server listening on http://localhost:' + PORT);
+        });
+    } else if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname + '/client/build/')))
+
+        // Routes the user to the React html
+        app.get('*', (req, res) => {
+            res.sendFile(__dirname + '/client/build/index.html')
+        });
+
+        server.listen(PORT, () => {
+            console.log('Server listening on nicksnpcs.herokuapp.com:' + PORT);
+        });
+    }
+} else {
     app.use(express.static(path.join(__dirname + '/client/build/')))
 
     // Routes the user to the React html
@@ -28,7 +41,8 @@ if (process.env.NODE_ENV === 'development') {
     });
 
     server.listen(PORT, () => {
-        console.log('Server listening on nicksnpcs.herokuapp.com:' + PORT);
+        console.log('Server listening on ' + process.env.PUBLIC_URL);
     });
+
 }
 
