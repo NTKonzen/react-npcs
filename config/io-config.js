69e867b022f93f9fa3bdb7f1bd20ae19f8716ac4
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-11-19 23:29:38 -0800

    added new allowed origin

--- config/io-config.js
+++ config/io-config.js
@@ -1,7 +1,7 @@
 module.exports = function (server) {
     return require('socket.io')(server, {
         cors: {
-            origin: ["http://localhost:3000", "http://localhost:3001", `https://nicksnpcs.herokuapp.com:${process.env.PORT}`, `http://nicksnpcs.herokuapp.com:${process.env.PORT}`],
+            origin: ["http://localhost:3000", "http://localhost:3001", `https://nicksnpcs.herokuapp.com:${process.env.PORT}`, `http://nicksnpcs.herokuapp.com:${process.env.PORT}`, `nicksnpcs.herokuapp.com:*`],
             methods: ["GET", "POST"],
             allowedHeaders: ["my-custom-header"],
             credentials: true
