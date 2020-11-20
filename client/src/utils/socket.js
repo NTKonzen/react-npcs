9dbcc04a75c02cb06357c914963f5f49adf838c2
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-11-19 19:51:23 -0800

    Added NODE_ENV handling to client

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,15 @@
 import io from "socket.io-client";
 import Cookies from "js-cookie";
 
-export const socket = io("http://localhost:3001", {
+let connectionString;
+
+if (process.env.NODE_ENV === 'production') {
+    connectionString = `nicksnpcs.herokuapp.com:${process.env.PORT}`
+} else if (process.env.NODE_ENV === 'development') {
+    connectionString = "http://localhost:3001"
+}
+
+export const socket = io(connectionString, {
     withCredentials: true,
     extraHeaders: {
         "my-custom-header": "abcd"
